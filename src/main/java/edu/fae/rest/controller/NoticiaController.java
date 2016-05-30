944b0c4bd691b7da45a008765d985453593e950f
Robson Luz <robson.luz@gmail.com>
2016-05-30 20:35:49 -0300

    CRUD completo de exemplo

--- src/main/java/edu/fae/rest/controller/NoticiaController.java
+++ src/main/java/edu/fae/rest/controller/NoticiaController.java
@@ -3,10 +3,13 @@ package edu.fae.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.fae.rest.model.Message;
 import edu.fae.rest.model.Noticia;
 import edu.fae.rest.repository.NoticiaRepository;
 
@@ -27,5 +30,31 @@ public class NoticiaController {
 	public List<Noticia> findAll() {
 		return noticiaRepository.findAll();
 	}
+	
+	/**
+	 * @return Retorna uma noticia por id
+	 */
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Noticia show(@PathVariable Long id) {
+		return noticiaRepository.findOne(id);
+	}
+	
+	/**
+	 * Insere uma notícia na base de dados
+	 */
+	@RequestMapping(value="", method=RequestMethod.POST)
+	public Noticia save(@RequestBody Noticia noticia) {
+		noticiaRepository.save(noticia);
+		return noticia;
+	}		
+	
+	/**
+	 * Remove uma notícia na base de dados
+	 */
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public Message delete(@PathVariable Long id) {
+		noticiaRepository.delete(id);
+		return Message.OK;
+	}	
 	
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -11,8 +11,27 @@ app.config(['$routeProvider', function($routerProvider){
 		
 		.when('/noticias', {
 			templateUrl: 'noticias-list.html',
-			controller: 'NoticiaListController'
+			controller: 'NoticiaController',
+			method: 'list'
 		})
+		
+		.when('/noticias/new', {
+			templateUrl: 'noticias-form.html',
+			controller: 'NoticiaController',
+			method: 'create'
+		})
+		
+		.when('/noticias/:id', {
+			templateUrl: 'noticias-show.html',
+			controller: 'NoticiaController',
+			method: 'show'
+		})		
+		
+		.when('/noticias/:id/edit', {
+			templateUrl: 'noticias-form.html',
+			controller: 'NoticiaController',
+			method: 'show'
+		})		
 	;
 }]);
 
@@ -22,8 +41,44 @@ app.factory('NoticiaService', function($resource) {
 	return $resource('/api/noticias/:id', {}, {});
 });
 
-//NoticiaListController
-app.controller('NoticiaListController', function($scope, NoticiaService) {
-	$scope.noticias = NoticiaService.query();	
+//NoticiaController
+app.controller('NoticiaController', function($scope, $routeParams, $route, $location, NoticiaService) {
+	
+	$scope.list = function() {
+		$scope.noticias = NoticiaService.query();	
+	}
+	
+	$scope.show = function() {
+		$scope.noticia = NoticiaService.get({"id": $routeParams.id});
+	}
+	
+	$scope.create = function() {
+		$scope.noticia = {};
+	}	
+	
+	$scope.edit = function() {
+		$scope.noticia = NoticiaService.get({"id": $routeParams.id});
+	}	
+	
+	$scope.save = function() {
+		NoticiaService.save($scope.noticia, function(noticia){
+			if(noticia) {
+				$location.path('/noticias/' + noticia.id);
+			}
+    	});
+	}
+	
+	$scope.remove = function() {
+		if(confirm('Confirma a Exclusão?')) {
+	    	NoticiaService.remove($scope.noticia, function(){
+	    		$location.path('/noticias');
+	    	});
+		}
+    }
+	
+	//Chama o método definido na rota
+	if($route.current.method){ 
+		$scope[$route.current.method]();
+	}
 });
 
