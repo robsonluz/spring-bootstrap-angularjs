88ba69d1a925014a0442fcaf238f4eca35de31c7
Robson Luz <robson.luz@gmail.com>
2016-06-01 23:24:19 -0300

    Ajuste M2

--- src/main/java/edu/fae/rest/NoticiasBootstrapApplication.java
+++ src/main/java/edu/fae/rest/NoticiasBootstrapApplication.java
@@ -1,21 +1,26 @@
 package edu.fae.rest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import edu.fae.rest.model.Noticia;
+import edu.fae.rest.repository.NoticiaRepository;
 
 @SpringBootApplication
 public class NoticiasBootstrapApplication {
 
-//	//Dados iniciais da base de dados
-//	@Bean
-//	CommandLineRunner runner(NoticiaRepository noticiaRepository){
-//		return args -> {
-//			noticiaRepository.save(new Noticia("Notícia 1", "Texto da Notícia 1"));
-//			noticiaRepository.save(new Noticia("Notícia 2", "Texto da Notícia 2"));
-//			noticiaRepository.save(new Noticia("Notícia 3", "Texto da Notícia 3"));
-//			noticiaRepository.save(new Noticia("Notícia 4", "Texto da Notícia 4"));
-//		};
-//	}	
+	//Dados iniciais da base de dados
+	@Bean
+	CommandLineRunner runner(NoticiaRepository noticiaRepository){
+		return args -> {
+			noticiaRepository.save(new Noticia("Notícia 1", "Texto da Notícia 1"));
+			noticiaRepository.save(new Noticia("Notícia 2", "Texto da Notícia 2"));
+			noticiaRepository.save(new Noticia("Notícia 3", "Texto da Notícia 3"));
+			noticiaRepository.save(new Noticia("Notícia 4", "Texto da Notícia 4"));
+		};
+	}	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(NoticiasBootstrapApplication.class, args);
